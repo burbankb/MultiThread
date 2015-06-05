2c0b58acdfa3ea054d2f5123b1078acc84bf9008
Brandyn Burbank <bur11012@byui.edu>
2015-06-05 02:18:02 -0600

    further work. Got threads working. Still working on file writing/reading

--- app/src/main/java/com/brandynburbank/multithread/MainActivity.java
+++ app/src/main/java/com/brandynburbank/multithread/MainActivity.java
@@ -1,12 +1,37 @@
 package com.brandynburbank.multithread;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.LogRecord;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private final String FILENAME = "theFile.txt";
+
+    private ProgressBar mProgress;
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
+
+    private Context context = getApplicationContext();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +59,81 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void writeFileHandler(View v) {
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressStatus = 0;
+
+        final File file = new File(context.getFilesDir(), FILENAME);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 10) {
+                    mProgressStatus++;
+
+                    try {
+                        BufferedWriter buf = new BufferedWriter(new FileWriter(file));
+                        buf.write("Wheat");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        Thread.sleep((long) 500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus * 10);
+                        }
+                    });
+                }
+            }
+        }).start();
+
+        file.Close();
+
+    }
+
+    public void readFileHandler(View v) {
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressStatus = 0;
+
+        final File file = new File(context.getFilesDir(), FILENAME);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (mProgressStatus < 10) {
+                    mProgressStatus++;
+
+                    try {
+                        BufferedReader buf = new BufferedReader(new FileReader(file));
+                        System.out.println(buf.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        Thread.sleep((long) 500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            mProgress.setProgress(mProgressStatus * 10);
+                        }
+                    });
+                }
+            }
+        }).start();
+
+        file.close();
+
     }
 }
